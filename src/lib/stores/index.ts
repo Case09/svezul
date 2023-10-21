a74847f7e166476fc23ce0bf22ab491545e9831d
Bojan Čoka <coka@fastmail.com>
2023-10-21 12:22:43 +0200

    Pull random tiles from a bag

--- src/lib/stores/index.ts
+++ src/lib/stores/index.ts
@@ -1,12 +1,30 @@
-import type { TileColor } from '$lib';
+import { tiles, type TileColor } from '$lib';
+import { shuffle, take, drop } from 'lodash';
 import { writable } from 'svelte/store';
 
 interface AzulStore {
+	bag: TileColor[];
 	tiles: TileColor[];
 }
 
+const numberOfTilesOfEachColor = 20;
+const initialBag: TileColor[] = shuffle(
+	Array(numberOfTilesOfEachColor)
+		.fill([...tiles])
+		.flat()
+);
+
+function takeTilesFrom(tiles: TileColor[]): [TileColor[], TileColor[]] {
+	const taken = take(tiles, 4);
+	const remaining = drop(tiles, 4);
+	return [taken, remaining];
+}
+
+const [initialTiles, bag] = takeTilesFrom(initialBag);
+
 const azulStore = writable(<AzulStore>{
-	tiles: []
+	bag,
+	tiles: initialTiles
 });
 
 export default azulStore;
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,10 @@
 <script lang="ts">
-	import { tiles, type TileColor } from '$lib';
 	import FactoryTile from '$lib/components/FactoryTile.svelte';
 	import azulStore from '$lib/stores/index';
-	import { onMount } from 'svelte';
-
-	onMount(() => {
-		randomizeTiles();
-	});
-
-	function randomizeTiles(): void {
-		const numberOfTiles = 4;
-		let randomTiles: TileColor[] = [];
-		for (let i = 0; i < numberOfTiles; i++) {
-			const idx = Math.floor(Math.random() * 5);
-			randomTiles.push(tiles[idx]);
-		}
-		$azulStore.tiles = randomTiles;
-	}
 </script>
 
 <div class="board-container h-screen w-screen p-2">
+	<h1>Tiles in bag: {$azulStore.bag.length}</h1>
 	<div class="board flex h-full w-full items-center justify-center text-5xl">
 		<FactoryTile />
 	</div>
