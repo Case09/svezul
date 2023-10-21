457b46f5900d644cea85400328cbd452851e76ed
Bojan Čoka <coka@fastmail.com>
2023-10-21 13:15:41 +0200

    Take fresh tiles from the bag every time a tile is picked

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,6 @@
 import { get } from 'svelte/store';
 import azulStore from './stores';
+import { drop, take } from 'lodash';
 
 export const tiles = ['blue', 'orange', 'red', 'black', 'white'] as const;
 
@@ -13,8 +14,19 @@ export const azulColors: Record<TileColor, string> = {
 	white: '#add0de'
 };
 
+export function takeTilesFrom(tiles: TileColor[]): [TileColor[], TileColor[]] {
+	const taken = take(tiles, 4);
+	const remaining = drop(tiles, 4);
+	return [taken, remaining];
+}
+
 export function pickTile(tile: TileColor) {
 	const azulStoreValue = get(azulStore);
 	const points = azulStoreValue.tiles.filter((t) => t === tile).length;
-	azulStore.update((s) => ({ ...s, score: s.score + points }));
+	const [newTiles, bag] = takeTilesFrom(azulStoreValue.bag);
+	azulStore.update((s) => ({
+		bag,
+		tiles: newTiles,
+		score: s.score + points
+	}));
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import { tiles, type TileColor } from '$lib';
+	import { takeTilesFrom, tiles, type TileColor } from '$lib';
 	import FactoryTile from '$lib/components/FactoryTile.svelte';
 	import azulStore, { numberOfTilesOfEachColor } from '$lib/stores/index';
-	import { drop, shuffle, take } from 'lodash';
+	import { shuffle } from 'lodash';
 	import { onMount } from 'svelte';
 
 	onMount(() => {
@@ -17,12 +17,6 @@
 		$azulStore.bag = bag;
 		$azulStore.tiles = initialTiles;
 	});
-
-	function takeTilesFrom(tiles: TileColor[]): [TileColor[], TileColor[]] {
-		const taken = take(tiles, 4);
-		const remaining = drop(tiles, 4);
-		return [taken, remaining];
-	}
 
 	$: console.log($azulStore);
 </script>
