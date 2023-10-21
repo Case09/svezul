f2aa62768bd49dca66f744e96d8bbecad0e0b2fe
Bojan Čoka <coka@fastmail.com>
2023-10-21 14:37:24 +0200

    Display a wall of tiles

---
+++ src/lib/components/Wall.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { tiles, type TileColor } from '$lib';
+	import { take, takeRight } from 'lodash';
+	import Tile from './Tile.svelte';
+
+	function rotate<T>(index: number, list: T[]): T[] {
+		const head = takeRight(list, index);
+		const tail = take(list, list.length - index);
+		return [...head, ...tail];
+	}
+
+	const rows: TileColor[][] = [];
+	for (let i = 0; i < 5; i++) {
+		rows.push(rotate(i, [...tiles]));
+	}
+</script>
+
+<div>
+	{#each rows as row}
+		<div class="flex">
+			{#each row as tile}
+				<Tile {tile} />
+			{/each}
+		</div>
+	{/each}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { takeTilesFrom, tiles, type TileColor } from '$lib';
 	import FactoryTile from '$lib/components/FactoryTile.svelte';
+	import Wall from '$lib/components/Wall.svelte';
 	import azulStore, { numberOfTilesOfEachColor } from '$lib/stores/index';
 	import { shuffle } from 'lodash';
 	import { onMount } from 'svelte';
@@ -21,12 +22,15 @@
 	$: console.log($azulStore);
 </script>
 
-<div class="h-screen w-screen p-2">
+<div class="flex h-screen w-screen flex-col p-2">
 	{#if $azulStore.bag.length && !$azulStore.gameOver}
 		<h1>Tiles in bag: {$azulStore.bag.length}</h1>
 		<h1>Score: {$azulStore.score}</h1>
 	{/if}
-	<div class="flex h-full w-full items-center justify-center text-5xl">
-		<FactoryTile />
+	<div class="flex grow items-center justify-center gap-4">
+		<div class="flex items-center justify-center text-5xl">
+			<FactoryTile />
+		</div>
+		<Wall />
 	</div>
 </div>
